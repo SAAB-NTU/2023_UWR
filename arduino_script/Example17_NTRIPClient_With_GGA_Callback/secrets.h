508fa8dbc5628a1de73cd53935377410b2bb87d2
Admin <abubakr.azam@live.com>
2023-09-15 17:02:07 +0800

    add ublox info

---
+++ arduino_script/Example17_NTRIPClient_With_GGA_Callback/secrets.h
@@ -0,0 +1,17 @@
+//Your WiFi credentials
+const char ssid[] = "dlink-0644";
+const char password[] =  "ksrwp56703";
+
+//RTK2Go works well and is free
+const char casterHost[] = "199.184.151.36"; 
+const uint16_t casterPort = 2101;
+const char casterUser[] = "MHUART01"; //User must provide their own email address to use RTK2Go
+const char casterUserPW[] = "1P9WTPJKZANK";
+const char mountPoint[] = "RTK_SNYU_32"; //The mount point you want to get data from
+
+//Emlid Caster also works well and is free
+//const char casterHost[] = "caster.emlid.com";
+//const uint16_t casterPort = 2101;
+//const char casterUser[] = "u99696"; //User name and pw must be obtained through their web portal
+//const char casterUserPW[] = "466zez";
+//const char mountPoint[] = "MP1979"; //The mount point you want to get data from
