5f7078c2776a5716b964f52a7805eabca458e91d
Admin <abubakr.azam@live.com>
2023-11-01 12:49:41 +0800

    Added polar to cartesian script

--- ros_ws/src/oculus_ros/tests/python/subscriber01.py
+++ ros_ws/src/oculus_ros/tests/python/subscriber01.py
@@ -9,9 +9,10 @@ import oculus_sonar.msg as oculus_msg
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import numpy as np
+import cv2
 cv_bridge = CvBridge()
 
-h, w = (512,512)
+h, w = (50,50)
 r = 2 * h - 1
 top = False
 angle = 130
@@ -22,10 +23,12 @@ def callback(data):
     data.data = []
     #print(data, flush=True)
 def callback_fan(data):
+    
     polar_img = cv_bridge.imgmsg_to_cv2(data, "bgr8")
+    polar_img=cv2.cvtColor(polar_img,cv2.COLOR_BGR2GRAY)
     polar_normal=1-(polar_img-np.min(polar_img))/(np.max(polar_img)-np.min(polar_img))
     polar_raised=polar_normal*255
-
+    rospy.loginfo(polar_img.shape)
     
     polar_raised=np.rot90(polar_raised,2)
     im_fan = np.ones((r, r), dtype=np.uint8)
@@ -37,12 +40,16 @@ def callback_fan(data):
         im_fan[(rows, cols)] = polar_raised[:, i//k]
     im_fan = im_fan[r//2:, :]
     im_out = np.flip(im_fan, axis=0)
-    fls_fan_pub.publish(im_out)
+    image_msg = cv_bridge.cv2_to_imgmsg(im_out, encoding="8UC1")
+
+        # Set the timestamp of the message (optional)
+    image_msg.header.stamp = rospy.Time.now()
+    fls_fan_pub.publish(image_msg)
 
 rospy.init_node('sonar_listener', anonymous=True)
 fls_fan_pub = rospy.Publisher('/fls_fan_img', Image, queue_size=1)
-rospy.Subscriber('/ping', oculus_msg.OculusPing, callback)
-rospy.Subscriber("/ping/image", Image, callback_fan)
+#rospy.Subscriber('/ping', oculus_msg.OculusPing, callback)
+rospy.Subscriber("/oculus_sonar/ping_image", Image, callback_fan)
 
 
 rospy.spin()
---
+++ ros_ws/src/oculus_ros/tests/python/test_image.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+def publish_image_from_file():
+    # Initialize the ROS node
+    rospy.init_node('image_publisher', anonymous=True)
+    
+    # Create a publisher for the 'ping_image' topic
+    image_pub = rospy.Publisher('/oculus_sonar/ping_image', Image, queue_size=1)
+    
+    # Initialize CvBridge for image conversion
+    bridge = CvBridge()
+
+    # Load the image from file (change the file path accordingly)
+    image_path = '/home/saab/Desktop/123.jpg'
+    image = cv2.imread(image_path)
+
+    # Check if the image was loaded successfully
+    if image is None:
+        rospy.logerr("Failed to load the image from file")
+        return
+
+    try:
+        # Create an Image message
+        image_msg = bridge.cv2_to_imgmsg(image, encoding="bgr8")
+
+        # Set the timestamp of the message (optional)
+        image_msg.header.stamp = rospy.Time.now()
+
+        # Publish the image message
+        image_pub.publish(image_msg)
+
+        rospy.loginfo("Published image to 'ping_image' topic")
+
+  
+
+    except rospy.ROSInterruptException:
+        pass
+    
+
+if __name__ == '__main__':
+    while(not rospy.is_shutdown()):
+        publish_image_from_file()
+    
+
